7ac2a76d55a1f11521440749eb519c21ad446659
finteger <nashassociatesinc@gmail.com>
2025-06-02 19:02:46 -0600

    Update

--- test_main.py
+++ test_main.py
@@ -5,4 +5,8 @@ def test_get_weather():
     assert get_weather(19) == "cold"
     assert get_weather(31) == "hot"
     
+def test_divide():
+    with pytest.raises(ValueError, match='Cannot divide by zero'):
+        divide(10, 0)
+    
     
